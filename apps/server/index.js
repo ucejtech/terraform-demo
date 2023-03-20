f6e804bdd1d77171b3b8e1653cd2d98ef3aed013
Uchenna Jeremiah <28508741+ucejtech@users.noreply.github.com>
2023-03-20 19:06:24 +0100

    Updated Backend

--- apps/server/index.js
+++ apps/server/index.js
@@ -15,16 +15,18 @@ app.use(express.json({ limit: '50mb' }));
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
 
-app.get('/', async (req, res) => {
+app.get('/api', async () => {
   res.status(200).json({
-    message: 'Hello from DALL.E!',
+    message: 'Hello from DALL.E!'
   });
 });
 
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(process.env.PORT || 3001, () => console.log('Server started on port 8080'));
+    app.listen(process.env.PORT || 3001, () =>
+      console.log('Server started on port 8080')
+    );
   } catch (error) {
     console.log(error);
   }
